409e509896d8ac2a8c02fe1610590304620aa97b
hunterwb <hunter@hunterwb.com>
2018-01-14 04:54:12 -0800

    Add Dimension times and div

--- src/main/java/org/kxtra/swing/dimension/Dimension.kt
+++ src/main/java/org/kxtra/swing/dimension/Dimension.kt
@@ -5,6 +5,26 @@ package org.kxtra.swing.dimension
 import java.awt.Dimension
 import java.awt.geom.Dimension2D
 
-operator fun Dimension.plus(other: Dimension2D) = Dimension(width + other.width.toInt(), height + other.height.toInt())
+operator fun Dimension.plus(other: Dimension2D): Dimension {
+    return Dimension(width + other.width.toInt(), height + other.height.toInt())
+}
 
-operator fun Dimension.minus(other: Dimension2D) = Dimension(width - other.width.toInt(), height - other.height.toInt())
+operator fun Dimension.minus(other: Dimension2D): Dimension {
+    return Dimension(width - other.width.toInt(), height - other.height.toInt())
+}
+
+operator fun Dimension.times(n: Double): Dimension {
+    return Dimension((width * n).toInt(), (height * n).toInt())
+}
+
+operator fun Dimension.times(n: Int): Dimension {
+    return Dimension(width * n, height * n)
+}
+
+operator fun Dimension.div(n: Double): Dimension {
+    return Dimension((width / n).toInt(), (height / n).toInt())
+}
+
+operator fun Dimension.div(n: Int): Dimension {
+    return Dimension(width / n, height / n)
+}
