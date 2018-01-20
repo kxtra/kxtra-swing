5d4b99b08e536d170e903b6860cb52e0fac10e12
hunterwb <hunter@hunterwb.com>
2018-01-19 17:48:51 -0800

    Remove ambiguous Font constructor

--- src/main/java/org/kxtra/swing/font/Font.kt
+++ src/main/java/org/kxtra/swing/font/Font.kt
@@ -3,20 +3,11 @@
 package org.kxtra.swing.font
 
 import java.awt.Font
-
-/**
- * @param[style] one of case-insensitive `"plain"`, `"bold"`, `"italic"`, or `"bolditalic"`
- *
- * @see[Font.decode]
- * @see[Font.deriveFont]
- */
-fun Font(name: String, style: String, size: Float): Font {
-    return Font.decode("$name-$style-${size.toInt()}").deriveFont(size)
-}
+import kotlin.math.roundToInt
 
 /**
  * @see[Font.deriveFont]
  */
 fun Font(name: String, style: Int, size: Float): Font {
-    return Font(name, style, size.toInt()).deriveFont(size)
+    return Font(name, style, size.roundToInt()).deriveFont(size)
 }
