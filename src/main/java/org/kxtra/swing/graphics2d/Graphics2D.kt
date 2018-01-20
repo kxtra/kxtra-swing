08b1617f4f7f8d7e249b741a978e650a2f791e3e
hunterwb <hunter@hunterwb.com>
2018-01-19 18:09:12 -0800

    Add Graphics2D create2D() with parameters

--- src/main/java/org/kxtra/swing/graphics2d/Graphics2D.kt
+++ src/main/java/org/kxtra/swing/graphics2d/Graphics2D.kt
@@ -9,7 +9,16 @@ import java.awt.geom.Point2D
 /**
  * @see[java.awt.Graphics.create]
  */
-fun Graphics2D.create2D(): Graphics2D = create() as? Graphics2D? ?: throw UnsupportedOperationException()
+fun Graphics2D.create2D(): Graphics2D {
+    return create() as? Graphics2D? ?: throw UnsupportedOperationException()
+}
+
+/**
+ * @see[java.awt.Graphics.create]
+ */
+fun Graphics2D.create2D(x: Int, y: Int, width: Int, height: Int): Graphics2D {
+    return create(x, y, width, height) as? Graphics2D? ?: throw UnsupportedOperationException()
+}
 
 /**
  * @see[TextLayout.draw]
