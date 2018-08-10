79f8e4b53a975781120a94fedc022018ed2ed6eb
hunterwb <hunter@hunterwb.com>
2018-08-09 22:28:53 -0700

    Add KeyEvent.keyStroke

--- src/main/java/org/kxtra/swing/keyevent/KeyEvent.kt
+++ src/main/java/org/kxtra/swing/keyevent/KeyEvent.kt
@@ -3,8 +3,14 @@
 package org.kxtra.swing.keyevent
 
 import java.awt.event.KeyEvent
+import javax.swing.KeyStroke
 
 /**
  * @see[KeyEvent.getKeyText]
  */
 val KeyEvent.keyText: String get() = KeyEvent.getKeyText(keyCode)
+
+/**
+ * @see[KeyStroke.getKeyStrokeForEvent]
+ */
+val KeyEvent.keyStroke: KeyStroke get() = KeyStroke.getKeyStrokeForEvent(this)
