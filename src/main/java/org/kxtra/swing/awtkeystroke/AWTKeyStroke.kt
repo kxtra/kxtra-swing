14cf6d3bc25a8ea07a80862165008174f191c384
hunterwb <hunter@hunterwb.com>
2018-08-14 17:05:42 -0700

    Remove keystroke mouse modifiers

--- src/main/java/org/kxtra/swing/awtkeystroke/AWTKeyStroke.kt
+++ src/main/java/org/kxtra/swing/awtkeystroke/AWTKeyStroke.kt
@@ -14,11 +14,3 @@ val AWTKeyStroke.isControlDown: Boolean get() = (modifiers and InputEvent.CTRL_D
 val AWTKeyStroke.isMetaDown: Boolean get() = (modifiers and InputEvent.META_DOWN_MASK) != 0
 
 val AWTKeyStroke.isAltGraphDown: Boolean get() = (modifiers and InputEvent.ALT_GRAPH_DOWN_MASK) != 0
-
-val AWTKeyStroke.isLeftMouseButtonDown: Boolean get() = (modifiers and InputEvent.BUTTON1_DOWN_MASK) != 0
-
-val AWTKeyStroke.isMiddleMouseButtonDown: Boolean get() = (modifiers and InputEvent.BUTTON2_DOWN_MASK) != 0
-
-val AWTKeyStroke.isRightMouseButtonDown: Boolean get() = (modifiers and InputEvent.BUTTON3_DOWN_MASK) != 0
-
-fun AWTKeyStroke.isMouseButtonDown(button: Int): Boolean = (modifiers and InputEvent.getMaskForButton(button)) != 0
